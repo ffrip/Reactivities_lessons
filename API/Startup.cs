e2cfaa04c0713ae187425ceb891951967a61b639
Frip <angryfrip@gmail.com>
2020-10-30 16:46:28 +0300

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,12 @@ namespace API
                 opt.UseSqlite(Configuration.GetConnectionString
                 ("DefaultConnection"));
             });
+
+            services.AddCors(opt =>{
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -49,6 +55,8 @@ namespace API
 
             app.UseAuthorization();
 
+            app.UseCors("CorsPolicy");
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
